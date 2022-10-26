beafb14af64a617b623b94eb1a184d5e4e766100
Stas D <iqpirat@gmail.com>
2022-10-26 17:34:01 +0200

    test: enable test

--- consumer/nats_test.go
+++ consumer/nats_test.go
@@ -12,102 +12,103 @@ import (
 
 	"github.com/skynet2/eventsourcing/common"
 	"github.com/skynet2/eventsourcing/consumer"
+	"github.com/skynet2/eventsourcing/publisher"
 )
 
-//	func TestNatsConsumer(t *testing.T) {
-//		con, err := nats.Connect(getNatsUrl(), nats.Timeout(30*time.Second), nats.ReconnectWait(30*time.Second))
-//		assert.NoError(t, err)
-//		js, err := con.JetStream()
-//		assert.NoError(t, err)
-//		sub := uuid.NewString()
-//
-//		_, err = js.AddStream(&nats.StreamConfig{
-//			Name:        sub,
-//			Description: "",
-//			Subjects:    []string{sub},
-//		}, nats.Context(context.TODO()))
-//		assert.NoError(t, err)
-//
-//		_, err = js.AddConsumer(sub, &nats.ConsumerConfig{
-//			Durable:    sub,
-//			Name:       sub,
-//			AckPolicy:  nats.AckExplicitPolicy,
-//			MaxDeliver: 10,
-//			AckWait:    100 * time.Second,
-//		}, nats.Context(context.TODO()))
-//		assert.NoError(t, err)
-//
-//		var receivedMessages []common.Event[eventStruct]
-//		firstInterceptorCalled := false
-//		secondInterceptorCalled := false
-//
-//		srv := consumer.NewNatsConsumer[eventStruct](js,
-//			consumer.NatsConsumerConfiguration{
-//				Concurrency:  1,
-//				ConsumerName: sub,
-//				Stream:       sub,
-//			},
-//			func(ctx context.Context, event *common.Event[eventStruct]) (consumer.ConfirmationType, error) {
-//				receivedMessages = append(receivedMessages, *event)
-//
-//				return consumer.ConfirmationTypeAck, nil
-//			}, consumer.WithNatsOptionInterceptors(func(next consumer.UnaryFunc) consumer.UnaryFunc {
-//				return func(ctx context.Context, request consumer.MessageRequest) (consumer.ConfirmationType, error) {
-//					if len(receivedMessages) == 0 {
-//						assert.False(t, firstInterceptorCalled)
-//						assert.False(t, secondInterceptorCalled)
-//						firstInterceptorCalled = true
-//					}
-//
-//					return next(ctx, request)
-//				}
-//			}, func(next consumer.UnaryFunc) consumer.UnaryFunc {
-//				return func(ctx context.Context, request consumer.MessageRequest) (consumer.ConfirmationType, error) {
-//					if len(receivedMessages) == 0 {
-//						assert.True(t, firstInterceptorCalled)
-//						assert.False(t, secondInterceptorCalled)
-//						secondInterceptorCalled = true
-//					}
-//					return next(ctx, request)
-//				}
-//			}))
-//
-//		assert.NoError(t, srv.ConsumeAsync())
-//
-//		pub := publisher.NewNatsPublisher[eventStruct](con, sub)
-//
-//		expected := []common.Event[eventStruct]{
-//			{
-//				Record: &eventStruct{Text: "123454321"},
-//				MetaData: common.MetaData{
-//					CrudOperation:       common.ChangeEventTypeCreated,
-//					CrudOperationReason: "created_1234",
-//				},
-//			},
-//			{
-//				Record: &eventStruct{Text: "321321321"},
-//				MetaData: common.MetaData{
-//					CrudOperation:       common.ChangeEventTypeUpdated,
-//					CrudOperationReason: "updated_12312",
-//				},
-//			},
-//		}
-//
-//		for _, e := range expected {
-//			assert.NoError(t, pub.Publish(context.TODO(), *e.Record, e.MetaData, nil))
-//		}
-//
-//		time.Sleep(5 * time.Second)
-//
-//		assert.Len(t, receivedMessages, 2)
-//		assert.Equal(t, expected, receivedMessages)
-//
-//		assert.NoError(t, srv.Close())
-//		assert.NoError(t, srv.Close())
-//		assert.True(t, firstInterceptorCalled)
-//		assert.True(t, secondInterceptorCalled)
-//	}
-//
+func TestNatsConsumer(t *testing.T) {
+	con, err := nats.Connect(getNatsUrl())
+	assert.NoError(t, err)
+	js, err := con.JetStream()
+	assert.NoError(t, err)
+	sub := uuid.NewString()
+
+	_, err = js.AddStream(&nats.StreamConfig{
+		Name:        sub,
+		Description: "",
+		Subjects:    []string{sub},
+	})
+	assert.NoError(t, err)
+
+	_, err = js.AddConsumer(sub, &nats.ConsumerConfig{
+		Durable:    sub,
+		Name:       sub,
+		AckPolicy:  nats.AckExplicitPolicy,
+		MaxDeliver: 10,
+		AckWait:    100 * time.Second,
+	})
+	assert.NoError(t, err)
+
+	var receivedMessages []common.Event[consumer.Spec]
+	firstInterceptorCalled := false
+	secondInterceptorCalled := false
+
+	srv := consumer.NewNatsConsumer[consumer.Spec](js,
+		consumer.NatsConsumerConfiguration{
+			Concurrency:  1,
+			ConsumerName: sub,
+			Stream:       sub,
+		},
+		func(ctx context.Context, event *common.Event[consumer.Spec]) (consumer.ConfirmationType, error) {
+			receivedMessages = append(receivedMessages, *event)
+
+			return consumer.ConfirmationTypeAck, nil
+		}, consumer.WithNatsOptionInterceptors(func(next consumer.UnaryFunc) consumer.UnaryFunc {
+			return func(ctx context.Context, request consumer.MessageRequest) (consumer.ConfirmationType, error) {
+				if len(receivedMessages) == 0 {
+					assert.False(t, firstInterceptorCalled)
+					assert.False(t, secondInterceptorCalled)
+					firstInterceptorCalled = true
+				}
+
+				return next(ctx, request)
+			}
+		}, func(next consumer.UnaryFunc) consumer.UnaryFunc {
+			return func(ctx context.Context, request consumer.MessageRequest) (consumer.ConfirmationType, error) {
+				if len(receivedMessages) == 0 {
+					assert.True(t, firstInterceptorCalled)
+					assert.False(t, secondInterceptorCalled)
+					secondInterceptorCalled = true
+				}
+				return next(ctx, request)
+			}
+		}))
+
+	assert.NoError(t, srv.ConsumeAsync())
+
+	pub := publisher.NewNatsPublisher[consumer.Spec](con, sub)
+
+	expected := []common.Event[consumer.Spec]{
+		{
+			Record: &consumer.Spec{ConsumerName: "test213"},
+			MetaData: common.MetaData{
+				CrudOperation:       common.ChangeEventTypeCreated,
+				CrudOperationReason: "created_1234",
+			},
+		},
+		{
+			Record: &consumer.Spec{ConsumerName: "321321321"},
+			MetaData: common.MetaData{
+				CrudOperation:       common.ChangeEventTypeUpdated,
+				CrudOperationReason: "updated_12312",
+			},
+		},
+	}
+
+	for _, e := range expected {
+		assert.NoError(t, pub.Publish(context.TODO(), *e.Record, e.MetaData, nil))
+	}
+
+	time.Sleep(5 * time.Second)
+
+	assert.Len(t, receivedMessages, 2)
+	assert.Equal(t, expected, receivedMessages)
+
+	assert.NoError(t, srv.Close())
+	assert.NoError(t, srv.Close())
+	assert.True(t, firstInterceptorCalled)
+	assert.True(t, secondInterceptorCalled)
+}
+
 //	func TestCloseWhileReading(t *testing.T) {
 //		con, err := nats.Connect(getNatsUrl(), nats.Timeout(30*time.Second), nats.ReconnectWait(30*time.Second))
 //		assert.NoError(t, err)
@@ -370,27 +371,28 @@ func TestOnNonExistingStream(t *testing.T) {
 //	con.Close()
 //}
 
-/*func TestExitOnStreamNotFound(t *testing.T) {
-	sub := uuid.NewString() + "exitNotFound"
-	con, err := nats.Connect(getNatsUrl(), nats.Timeout(30*time.Second), nats.ReconnectWait(30*time.Second))
-	assert.NoError(t, err)
-	js, err := con.JetStream()
-	assert.NoError(t, err)
+/*
+	func TestExitOnStreamNotFound(t *testing.T) {
+		sub := uuid.NewString() + "exitNotFound"
+		con, err := nats.Connect(getNatsUrl(), nats.Timeout(30*time.Second), nats.ReconnectWait(30*time.Second))
+		assert.NoError(t, err)
+		js, err := con.JetStream()
+		assert.NoError(t, err)
 
-	srv := consumer.NewNatsConsumer[consumer.Spec](js,
-		consumer.NatsConsumerConfiguration{
-			Concurrency:  1,
-			ConsumerName: sub,
-			Stream:       sub,
-		},
-		func(ctx context.Context, event *common.Event[consumer.Spec]) (consumer.ConfirmationType, error) {
-			return consumer.ConfirmationTypeAck, nil
-		}, consumer.WithNatsOptionExitOnStreamNotFound(true))
+		srv := consumer.NewNatsConsumer[consumer.Spec](js,
+			consumer.NatsConsumerConfiguration{
+				Concurrency:  1,
+				ConsumerName: sub,
+				Stream:       sub,
+			},
+			func(ctx context.Context, event *common.Event[consumer.Spec]) (consumer.ConfirmationType, error) {
+				return consumer.ConfirmationTypeAck, nil
+			}, consumer.WithNatsOptionExitOnStreamNotFound(true))
 
-	assert.ErrorContains(t, srv.ConsumeAsync(), "nats: stream not found")
-	assert.NoError(t, srv.Close())
-	con.Close()
-}
+		assert.ErrorContains(t, srv.ConsumeAsync(), "nats: stream not found")
+		assert.NoError(t, srv.Close())
+		con.Close()
+	}
 */
 func getNatsUrl() string {
 	if env := os.Getenv("JETSTREAM_HOST"); len(env) > 0 {
